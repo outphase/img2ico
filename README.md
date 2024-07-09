7e80d2c6c6009ac588f7668b5063f887c63a93d5
outphase <leonardo.grisoli@gmail.com>
2024-07-09 23:01:43 +0200

    updated documentation

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Welcome to img2ico, a windows icon making utility.
 Add `img2ico.exe` to your `PATH` and execute it with the image you wish to
 convert as the first and only argument:
 
-``` 
-img2ico <image name>.png
-```
+``` img2ico <image path>.png ```
 
 This will create a .ico file in the same directory with the same name as the
 source file.
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let image_name = args.get(1).unwrap_or_else(|| {
-        eprintln!(" Please provide a .png image\n  -> png2ico <image name>.png");
+        eprintln!(" Please provide a .png image\n  -> png2ico <image path>.png");
         process::exit(1);
     });
     if let Err(e) = img2ico::convert_image(image_name) {
